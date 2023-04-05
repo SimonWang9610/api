a22ad2eba3971305a6c5b265871b14bc4cab8ede
SimonWang9610 <dengpan9610.wang@gmail.com>
2023-04-05 16:28:07 -0400

    support fallback encoding for ApiResponse

--- example/event_source_example.dart
+++ example/event_source_example.dart
@@ -1,45 +1,58 @@
 import 'dart:convert';
 import 'package:simple_http_api/simple_http_api.dart';
 
+import 'key.dart';
+
 void main() async {
   final headers = {
     "Content-Type": "application/json",
-    "Authorization":
-        "Bearer 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",
+    "Authorization": "Bearer $apiKey",
   };
 
   final url = Uri.parse("https://api.openai.com/v1/chat/completions");
+
+  final template = 'write a chinese poem. do not translate.';
 
   final data = {
     "model": "gpt-3.5-turbo",
     "messages": [
       {
         "role": "user",
-        "content": "what is the difference between you and gpt4",
+        "content": template,
       }
     ],
-    "max_tokens": 2048,
+    "max_tokens": 1024,
     "stream": true,
   };
 
-  final eventSource = EventSource(url, ApiMethod.post);
-  eventSource.setHeaders(headers);
-
-  // final cancelToken = TimingToken(Duration(seconds: 5));
-  final stream = eventSource.send(body: json.encode(data));
-  // final stream = eventSource.send(cancelToken: cancelToken);
-
-  stream.listen(
-    (event) {
-      if (eventSource.isWeb) {
-        print(event.chunk as String);
-      } else {
-        final encoding = event.getEncoding();
-
-        print(encoding.decode(event.chunk as List<int>));
-      }
-    },
-    onError: (err) => print(err),
-    onDone: eventSource.close,
+  final result = await Api.post(
+    url,
+    headers: headers,
+    body: json.encode(data),
+    responseEncoding: utf8,
   );
+  print(result.encoding);
+
+  print(result.body);
+
+  // final eventSource = EventSource(url, ApiMethod.post);
+  // eventSource.setHeaders(headers);
+
+  // // final cancelToken = TimingToken(Duration(seconds: 5));
+  // final stream = eventSource.send(body: json.encode(data));
+  // // final stream = eventSource.send(cancelToken: cancelToken);
+
+  // stream.listen(
+  //   (event) {
+  //     if (eventSource.isWeb) {
+  //       print(event.chunk as String);
+  //     } else {
+  //       final encoding = event.getEncoding();
+
+  //       print(encoding.decode(event.chunk as List<int>));
+  //     }
+  //   },
+  //   onError: (err) => print(err),
+  //   onDone: eventSource.close,
+  // );
 }
--- example/upload_example.dart
+++ example/upload_example.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:isolate';
 import 'dart:convert';
 import 'package:simple_http_api/simple_http_api.dart';
 
@@ -44,10 +43,10 @@ Future<void> _uploadSingle(String path) async {
 Future<ApiResponse> _uploadMulti(OnProgressCallback? onProgress) async {
   final url = Uri.parse("http://127.0.0.1:8080/upload/multi");
   final file1 =
-      await FormData.fileFromPath("./assets/demo.mp4", field: "multi");
+      await FormData.fileFromPath("./assets/demo.mp4", field: "first");
 
-  // final file2 =
-  //     await FormData.fileFromPath("./assets/demo.png", field: "multi");
+  final file2 =
+      await FormData.fileFromPath("./assets/demo.png", field: "first");
 
   final formData = FormData();
 
@@ -56,7 +55,6 @@ Future<ApiResponse> _uploadMulti(OnProgressCallback? onProgress) async {
 
   formData.addFields({
     "upload": "test",
-    "delay": "delay",
   });
 
   return Api.upload(
